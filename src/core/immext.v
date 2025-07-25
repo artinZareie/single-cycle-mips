cc2d5a23ae63a0605a14106d3f51c5b53023674e
Artin Zarei <artin.zareie@yahoo.com>
2025-07-25 09:26:33 +0330

    Nothing in particular.

---
+++ src/core/immext.v
@@ -0,0 +1,13 @@
+/*
+ * @file src/core/immext.v
+ * @author Artin Zarei/Mohsen Mirzaei
+ * @brief signed and unsigned extension.
+ */
+
+module ImmExt (
+  input wire [15:0] immediate
+);
+
+  
+
+endmodule
